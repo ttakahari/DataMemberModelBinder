e7b90afd267e5ef36655a858f7426dd0f67cef65
t.takahari <taiga1018@gmail.com>
2019-02-10 23:42:54 +0900

    add ASP.NET MVC demo project.

---
+++ demo/AspNetMvcSample/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using AspNetMvcSample.Models;
+using System.Web.Mvc;
+
+namespace AspNetMvcSample.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Get(DemoFormModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Post(DemoFormModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ demo/AspNetMvcSample/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using DataMemberModelBinder.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AspNetMvcSample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ModelBinderProviders.BinderProviders.Insert(0, new DataMemberModelBinderProvider());
+        }
+    }
+}
